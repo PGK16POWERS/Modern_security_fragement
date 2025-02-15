1ac0dd479574e067f4b99482504b96be387d9567
programmer Kartier <progrmmerKartier@gmail.com>
2025-02-15 20:16:36 +0200

    TouchD

--- app/src/main/java/com/example/modernsecurity/Login.kt
+++ app/src/main/java/com/example/modernsecurity/Login.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 class Login : Fragment() {
+
+    private lateinit var emailInputField : EditText
+    private lateinit var passwordInputField : EditText
+    private lateinit var loginButton : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +22,11 @@ class Login : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        // ACCESS ALL THE INPUT FIELDS + LOGIN BUTTON
+        emailInputField = view.findViewById(R.id.email_input)
+        passwordInputField = view.findViewById(R.id.password_input)
+        loginButton = view.findViewById(R.id.login_button)
 
         val redirectToCreateAcc = view.findViewById<TextView>(R.id.redirect_to_create_acc)
         val forgotPassword = view.findViewById<TextView>(R.id.forgot_password)
@@ -30,5 +41,12 @@ class Login : Fragment() {
 
         return view
     }
+
+    private fun checkInputFields() {
+        if (emailInputField.text.length < 1) {
+            loginButton.setBackgroundResource(R.drawable.disabled_button)
+        }
+    }
+
 
 }
